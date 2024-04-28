c2e16c04a751d46e36a6badd72df7028b2f345e6
Mani0071 <124029912+Mani0071@users.noreply.github.com>
2024-04-28 18:48:30 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,8 +1,11 @@
+#connecting MYSQL with python
 import mysql.connector
 con=mysql.connector.connect(host='localhost',user='root',passwd='Mani123',database='student')
+#checking connected succesfully or not
 if con.is_connected():
     print('connected succesfully')
 
+#funtions for inserting a data into a table
 def insert(name, age, city):
     res = con.cursor()
     sql = "insert into users (name,age,city) values (%s,%s,%s)"
@@ -11,7 +14,7 @@ def insert(name, age, city):
     con.commit()
     print("Data Insert Success")
 
-
+#functions for update the existing data in a table
 def update(name, age, city,id):
     res = con.cursor()
     sql = "update users set name=%s,age=%s,city=%s where id=%s"
@@ -20,8 +23,7 @@ def update(name, age, city,id):
     con.commit()
     print("Data Update Success")
 
-
-
+#functions for selecting a particular column in a table
 def select():
     res = con.cursor()
     sql = "SELECT ID,NAME,AGE,CITY from users"
@@ -29,7 +31,7 @@ def select():
     result = res.fetchall()
     print(tabulate(result, headers=["ID", "NAME", "AGE", "CITY"]))
 
-
+#functions for delete the unwanted data
 def delete(id):
     res = con.cursor()
     sql = "delete from users where id=%s"
@@ -38,8 +40,7 @@ def delete(id):
     con.commit()
     print("Data Delete Success")
 
-
-
+#using loop to select the choice
 while True:
     print("1.Insert Data")
     print("2.Update Data")
@@ -47,22 +48,27 @@ while True:
     print("4.Delete Data")
     print("5.Exit")
     choice = int(input("Enter Your Choice : "))
+#calling a functions insert
     if choice == 1:
         name = input("Enter Name : ")
         age = int(input("Enter Age : "))
         city = input("Enter City : ")
         insert(name, age, city)
+#calling a functions update
     elif choice == 2:
         id = input("Enter The Id : ")
         name = input("Enter Name : ")
         age = input("Enter Age : ")
         city = input("Enter City : ")
         update(name, age, city,id)
+#calling a functions select
     elif choice == 3:
         select()
+#invoking a functions delete
     elif choice == 4:
         id = input("Enter The Id to Delete : ")
         delete(id)
+#exiting the loop if no choice is selected
     elif choice == 5:
         quit()
     else:
